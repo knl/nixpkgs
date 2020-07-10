fe672e921aabc40d586586e986654696ed390136
Nikola Knežević <nikola@knezevic.ch>
2020-07-10 12:55:25 +0200

    Revert "ruamel: moved from bitbucket to sourceforge"
    This reverts commit c3b5bdaba015f9fd1b8afff4ccee41ba29b6c655.

--- pkgs/development/python-modules/ruamel_yaml_clib/default.nix
+++ pkgs/development/python-modules/ruamel_yaml_clib/default.nix
@@ -1,6 +1,6 @@
 { stdenv
 , buildPythonPackage
-, fetchhg
+, fetchFromBitbucket
 , ruamel_base
 , ruamel_ordereddict
 , isPy3k
@@ -10,8 +10,9 @@ buildPythonPackage rec {
   pname = "ruamel.yaml.clib";
   version = "0.2.0";
 
-  src = fetchhg {
-    url = "http://hg.code.sf.net/p/ruamel-yaml-clib/code";
+  src = fetchFromBitbucket {
+    owner = "ruamel";
+    repo = "yaml.clib";
     rev = version;
     sha256 = "0kq6zi96qlm72lzj90fc2rfk6nm5kqhk6qxdl8wl9s3a42b0v6wl";
   };
@@ -21,7 +22,7 @@ buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "YAML parser/emitter that supports roundtrip preservation of comments, seq/map flow style, and map key order";
-    homepage = https://sourceforge.net/projects/ruamel-yaml-clib/;
+    homepage = https://bitbucket.org/ruamel/yaml;
     license = licenses.mit;
   };
 
