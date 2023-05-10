31deb80ecfe2613dbe69babfd50de244fff6c6f7
Bob van der Linden <bobvanderlinden@gmail.com>
2023-05-10 14:22:12 +0200

    espanso: 0.7.3 -> 2.1.8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4736,8 +4736,7 @@ with pkgs;
   eschalot = callPackage ../tools/security/eschalot { };
 
   espanso = callPackage ../applications/office/espanso {
-    inherit (darwin.apple_sdk.frameworks) AppKit Cocoa Foundation;
-    openssl = openssl_1_1;
+    inherit (darwin.apple_sdk.frameworks) AppKit Cocoa Foundation IOKit;
   };
 
   esphome = callPackage ../tools/misc/esphome { };
